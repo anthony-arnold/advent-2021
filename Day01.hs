08c4887752bf44900ae50517715e6205c6de72e6
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2021-12-02 10:05:30 +1000

    Update Day01.hs

--- Day01.hs
+++ Day01.hs
@@ -12,35 +12,19 @@ toList = do
             where
                 m = read l :: Int
 
--- Determine the next total by comparing the current window sum
--- with the next sum and increasing the total if needed.
--- x: next window sum
--- y: current total
--- z: current window sum
--- equals: (next total, next window sum)
-increased :: Int -> (Int, Int) -> (Int, Int)
-increased x (y, z)
-    | x > z = (y + 1, x)
-    | otherwise = (y, x)
-
 -- Compute the total number of window sum increases over a list.
 -- w: window size
 -- l: list to inspect
--- p: 2-tuple of previous total and previous window sum
-increases :: Int -> [Int] -> (Int, Int) -> (Int, Int)
+-- p: previous total 
+increases :: Int -> [Int] -> Int -> Int
 increases w l p
-    | length l < w = p
-    | otherwise = increases w r i
-        where
-            i = increased s p
-            s = sum $ take w l
-            r = tail l
+    | length l < (w + 1) = p
+    | (l !! w) > (head l) = increases w (tail l) p+1
+    | otherwise = increases w (tail l) p
     
 -- Short-hand to get the answer (discards temporaries).
-answer w l = r
-    where
-        (r, _) = increases w l (0, m)
-        m = maxBound :: Int
+answer :: Int -> [Int] -> Int
+answer w l = increases w l 0
 
 main = do
     l <- toList
